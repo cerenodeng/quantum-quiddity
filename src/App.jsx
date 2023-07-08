83d5c8983b28cd5bd6f7741910e64fcb8a8d9f82
Cereno Deng <cereno.deng@outlook.com>
2023-07-08 18:46:56 +0800

    delete three fiber

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
-import { Canvas } from '@react-three/fiber'
-import './App.css'
+import './App.css';
 
-function App() {
-
-  return (
-    <div id="canvas-container">
-      <Canvas>
-        <mesh>
-          <boxGeometry args={[2, 4, 6]} />
-          <meshStandardMaterial />
-          <ambientLight intensity={0.1} />
-          <directionalLight color="red" position={[0, 0, 5]} />
-        </mesh>
-      </Canvas>
-    </div>
-  )
+export default function App() {
+	return <div></div>;
 }
-
-export default App
